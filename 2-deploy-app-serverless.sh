cca22c6b57a0b31896c66d7c5759131259d8664b
Razvan Ristea <ristearzvn@yahoo.com>
2021-01-27 22:38:53 +0000

    Update script to delete the serverless app

--- 2-deploy-app-serverless.sh
+++ 2-deploy-app-serverless.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Get DB host from the Stack output.
+DB_HOST=$(aws cloudformation describe-stacks --stack-name app-private-db --query "Stacks[0].Outputs[?OutputKey=='PrivateServerlessDB'].OutputValue" --output text)
+
 echo "Deploy S3 bucket"
 ./create-stack.sh app-serverless/s3-bucket.yml
 
-# Get DB host from the Stack output.
-DB_HOST=$(aws cloudformation describe-stacks --stack-name app-private-db --query "Stacks[0].Outputs[?OutputKey=='PrivateServerlessDB'].OutputValue" --output text)
+echo "Deploy DB secret"
+./create-stack.sh app-serverless/secret-private-db.yml
 
 echo "Build Retriever"
 # Build the Retriever, and replace the host DB in the properties files.
@@ -15,9 +18,6 @@ aws s3 cp app-serverless/retriever/target/retriever-1.0-SNAPSHOT.jar s3://app-se
 
 echo "Deploy Retriever Lambda"
 ./create-stack.sh app-serverless/lambda-retriever.yml
-
-echo "Deploy DB secret"
-./create-stack.sh app-serverless/secret-private-db.yml
 
 echo "Build Exporter"
 mkdir ./app-serverless/exporter/tmp
--- 3-delete-app-serverless.sh
+++ 3-delete-app-serverless.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo "Delete Workflow StepFunction"
+./delete-stack.sh app-serverless/workflow-stepfunction.yml
+echo "Delete Exporter Lambda"
+./delete-stack.sh app-serverless/lambda-exporter.yml
+echo "Delete Retriever Lambda"
+./delete-stack.sh app-serverless/lambda-retriever.yml
+echo "Delete DB secret"
+./delete-stack.sh app-serverless/secret-private-db.yml
+echo "Remove all objects from S3 bucket"
+aws s3 rm s3://app-serverless-s3-bucket-s3bucket --recursive
+echo "Delete S3 bucket"
+./delete-stack.sh app-serverless/s3-bucket.yml
